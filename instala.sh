f06ff00eab5ddf762d9a6e8d32f08e70413f65ff
Eddy Jhoel <megapc18@gmail.com>
2020-06-03 10:28:08 -0500

    update instala.sh

--- instala.sh
+++ instala.sh
@@ -172,7 +172,7 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/EddyJhoel/gitpy/master/Install/trans &> /dev/null
 msg -bar2
-msg -ama "EDDYJHOEL-SCRIP BOOSTER"
+msg -ama "EDDYJHOEL-SCRIP FLIX"
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }

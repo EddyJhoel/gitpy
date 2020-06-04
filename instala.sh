e69eec0ed499ccd078bd4cafcc3b8200d2db2bbf
Eddy Jhoel <megapc18@gmail.com>
2020-06-03 19:47:39 -0500

    update instala.sh

--- instala.sh
+++ instala.sh
@@ -208,6 +208,7 @@ if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq|grep "KEY INVALIDA!") 
    mv -f /etc/bash.bashrc.2 /etc/bash.bashrc
    echo "${SCPdir}/menu" > /usr/bin/menu && chmod +x /usr/bin/menu
    echo "${SCPdir}/menu" > /usr/bin/adm && chmod +x /usr/bin/adm
+   echo "${SCPdir}/menu" > /usr/bin/py && chmod +x /usr/bin/py
    inst_components
    echo "$Key" > ${SCPdir}/key.txt
    [[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}   

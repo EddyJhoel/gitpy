e9e85cbdd802d6f8204c32102db9554dda6f3a86
Eddy Jhoel <megapc18@gmail.com>
2020-06-04 23:14:07 -0500

    update nload.sh

--- request/nload.sh
+++ request/nload.sh
@@ -2,8 +2,7 @@
 Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
 Block > /dev/null 2>&1
 clear
-cowsay -f eyes "Con esta herramienta puedes ver el trafico de red...." | lolcat 
-figlet ..dankelthaher.. | lolcat
+cowsay -f eyes "Con esta herramienta puedes ver el trafico de red...." 
 echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
 echo -e "\033[1;31mPARA SALIR DEL PANEL PRESIONE CTLR+C \e[0m"
 sleep 5s

4d41b4b4c2ae3095b9dae1066b1a50754ddad03a
Eddy Jhoel <megapc18@gmail.com>
2020-06-30 14:37:31 -0500

    Update MonitorPy.sh

--- MonitorPy.sh
+++ MonitorPy.sh
@@ -20,16 +20,19 @@ echo "---";
 
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 
-echo "Usuarios en OpenSSH";
 
-echo "---";
+
+echo "Usuarios [OpenSSH]";
+echo "[x]";
 for PID in "${data[@]}"
 do
         #echo "check $PID";
-		NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
-		USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
-		IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
-        if [ $NUM -eq 1 ]; then
-                echo "$PID - $USER - $IP";
+        NUM2=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+        USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+        IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM2 -eq 1 ]; then
+                echo "[PID] $PID - [Usuario]: $USER";
+                echo "[TOTAL en OpenSSH]: $NUM2";
         fi
+done
 done
